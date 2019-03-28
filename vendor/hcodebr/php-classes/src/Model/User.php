14ad68d2c0da67182467b1e975061f5dafe8f0c8
Gustavo Dauer <gustavo.dauer@webjump.com.br>
2019-03-28 16:01:09 -0300

    Criado sistema de recuperação de senha via e-mail com chave criptografada

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -3,12 +3,15 @@
 namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
+use Hcode\Mailer;
 use \Hcode\Model;
 
 class User extends Model {
 
     const SESSION = "User";
-    
+    const SECRET = 'Hcode';
+    const SECRET_IV = 'Webjump';
+
     public static function login($login, $password)
     {
         
@@ -157,6 +160,112 @@ class User extends Model {
             ":iduser"=>$this->getiduser()
         ));
         
+    }
+
+    public static function getForgot ($email)
+    {
+
+        $sql = new Sql();
+        
+        $results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email;", array(
+            ":email"=>$email
+        ));
+        
+        if(count($results) === 0)
+        {
+            throw new \Exception("Não foi possível recuperar a senha");
+        } else {
+
+            $data = $results[0];
+
+            $resultsRecovery = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+                ":iduser"=>$data['iduser'],
+                ":desip"=>$_SERVER['REMOTE_ADDR']
+            ));
+
+            if(count($resultsRecovery) === 0)
+            {
+
+                throw new \Exception("Não foi possível recuperar a senha756");
+
+            } else {
+
+                $dataRecovery = $resultsRecovery[0];
+
+                $key = pack('a16', User::SECRET);
+                $key_IV = pack('a16', User::SECRET_IV);
+
+                $code = base64_encode(openssl_encrypt($dataRecovery["idrecovery"], 'AES-128-CBC', $key, 0, $key_IV));
+
+                $link = "http://seudominio2.com/admin/forgot/reset?code=$code";
+
+                $mailer = new Mailer($data['desemail'], $data['desperson'] ,"Redefinir senha da Webjump Store" ,"forgot", array(
+                   "name"=>$data['desperson'],
+                   "link"=>$link
+                ));
+
+                $mailer->send();
+
+                return $data;
+
+            }
+            
+        }
+
+    }
+
+    public static function validForgotDecrypt($code)
+    {
+        $sql = new Sql();
+
+
+        $key = pack('a16', User::SECRET);
+        $key_IV = pack('a16', User::SECRET_IV);
+
+        $code = base64_decode($code);
+
+        $idrecovery = openssl_decrypt($code, 'AES-128-CBC', $key, 0, $key_IV);
+
+        
+        $results = $sql->select("
+          SELECT * 
+          FROM tb_userspasswordsrecoveries a
+          INNER JOIN tb_users b USING(iduser) 
+          INNER JOIN tb_persons c USING(idperson) 
+          WHERE 
+                a.idrecovery = :idrecovery 
+            AND 
+                a.dtrecovery IS NULL 
+            AND 
+                DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();", array(
+                    ":idrecovery"=>$idrecovery
+
+        ));
+
+        if(count($results) === 0)
+        {
+
+            var_dump($results);
+            throw new \Exception("Não foi possível recupera a senha.");
+
+
+        } else {
+
+            return $results[0];
+
+        }
+
+    }
+
+    public static function setForgotUsed($idrecovery)
+    {
+
+        $sql = new Sql();
+        
+        $sql->select("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+            ":idrecovery"=>$idrecovery
+        ));
+
     }
 
     
