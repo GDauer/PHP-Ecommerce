6222bdcf02c7090e28eb5315db41632d859f4131
Gustavo Dauer <gustavo.dauer@hotmail.com>
2019-04-01 11:41:54 -0300

    Relação Categoria x Produto

--- admin-categories.php
+++ admin-categories.php
@@ -2,6 +2,7 @@
 
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Product;
 use \Hcode\Model\Category;
 
 $app->get('/admin/categories', function() {
@@ -18,20 +19,6 @@ $app->get('/admin/categories', function() {
     ]);
 });
 
-
-$app->get('/categories/:idcategory', function ($idcategory) {
-
-    $category = new Category();
-
-    $category->get((int)$idcategory);
-
-    $page = new Page();
-
-    $page->setTpl("category", [
-        "category"=>$category->getValues(),
-        "products"=>[]
-    ]);
-});
 
 $app->get('/admin/categories/create', function() {
 
@@ -101,6 +88,66 @@ $app->post('/admin/categories/:idcategory', function($idcategory) {
 
     header("Location: /admin/categories");
     exit;
+});
+
+$app->get('/admin/categories/:idcategory/products', function($idcategory) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-products", [
+
+        'category'=>$category->getValues(),
+        'productsRelated'=>$category->getProducts(),
+        'productsNotRelated'=>$category->getProducts(false)
+    ]);
+});
+
+$app->get('/admin/categories/:idcategory/products/:idproduct/add', function($idcategory, $idproduct) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $category->addProduct($product);
+    
+    header("Location: /admin/categories/". $idcategory ."/products");
+    exit;
+
+});
+
+$app->get('/admin/categories/:idcategory/products/:idproduct/remove', function($idcategory, $idproduct) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $category->removeProduct($product);
+
+    header("Location: /admin/categories/". $idcategory ."/products");
+    exit;
+
 });
 
 ?>
